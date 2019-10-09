32b26b8c1e7bffb13d78f9a5c05b9a10b1058359
Perms92 <permso@stud.ntnu.no>
2019-10-09 13:29:01 +0200

    Fungerende startKast()

--- Utkast 1 Yatzy/script.js
+++ Utkast 1 Yatzy/script.js
@@ -1,14 +1,15 @@
 //Oppretter første kast hver runde
 function startKast(){
-  var mitt_kast =[];
+  var mitt_kast = [];
   //For hver terning
-  for (i = 0; i < 5; i++){
-    Math.floor(Math.random() * 6) +1);  //sjekk denne
+  for (i = 0; i <= 4; i++){
+    mitt_kast[i] = Math.round(Math.random()*(6-1)+1);  //sjekk denne
   }
   //Returnerer arrayet med 5 terninger med random verdier
-  return mitt_kast;
+  // return mitt_kast;
+  document.getElementById("kast").innerHTML = mitt_kast;
 }
-
+/*
 //Tar inn forrige kast og hvilke indekser som skal kastes på nytt
 function nyttKast(innListe, indekser){
    var utListe = innListe;
@@ -19,3 +20,16 @@ function nyttKast(innListe, indekser){
   //Returnerer nytt kast
   return utListe;
 }
+
+// Definerer hvilke indekser som skal kastes på nytt
+function nytt_kast_indexer(){
+
+
+}
+
+function nyTerning(liste, posisjon){
+  var utListe = liste;
+  utListe[posisjon] = Math.floor(Math.random() * 6) +1);
+}
+return utListe;
+*/
