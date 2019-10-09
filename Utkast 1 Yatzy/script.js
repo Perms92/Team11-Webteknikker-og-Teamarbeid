255287a2856d1a9ecc4496bee13f6524ea9715a0
edvardotoft <edvardotoft@gmail.com>
2019-10-09 12:47:51 +0200

    5 tall

--- Utkast 1 Yatzy/script.js
+++ Utkast 1 Yatzy/script.js
@@ -1,8 +1,19 @@
+var arr = [];
 
-function startKast(){
-
+function fyllarray() {
+  for(var h=0; h<=4; h++){
+    arr[h]=Math.round(Math.random()*(6-1)+1);
+  }
 }
 
-function nyttKast(){
+function visarray() {
+  if(typeof(arr[4]) == "number"){
+    for(h=0; h<=4; h++);{
+  document.getElementById("utdata").innerHTML += arr + " ";
+  }
+}
 
+else {
+  alert("Du må generere tall først");
+  }
 }
