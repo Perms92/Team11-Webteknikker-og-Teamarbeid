ed3595ff7dfdcdf3ab0ccd222d8f9847775cd641
Perms92 <permso@stud.ntnu.no>
2019-10-10 11:32:31 +0200

    Update yatzy.js

--- Utkast 1 Yatzy/yatzy.js
+++ Utkast 1 Yatzy/yatzy.js
@@ -16,6 +16,9 @@ function startKast(){
     // Returnerer arrayet med 5 terninger med random verdier
     document.getElementById("kast").innerHTML = mitt_kast;
   }
+  else {
+    console.log("Bare første kast her");
+  }
 }
 
 
@@ -25,15 +28,12 @@ function nyttKast(indekser){
   if (antallKast > 0 && antallKast < 3) {
     for (i = 0; i <= 4; i++){
       if (indekser[i] == 0){
-        console.log("Test: indekser [0] " + indekser[i]);
+        //console.log("Test: indekser [0] " + indekser[i]);
         mitt_kast[i] = Math.floor((Math.random() * 6) + 1);
-      }
-      else {
-        console.log("Test: indekser [1] " + indekser[i]);
       }
     }
     antallKast += 1;
-    console.log("Test: antallKast " + antallKast);
+    //console.log("Test: antallKast " + antallKast);
     if (antallKast == 3) {
       antallKast = 0;
       console.log("Du har nå kastet 3 ganger, start på nytt");
@@ -52,44 +52,44 @@ function nytt_kast_indexer(){
   var nyeTerninger = [];
   if (document.getElementById("check1").checked){
     nyeTerninger[0] = 1;
-    console.log("Test: Terning 1");
+    //console.log("Test: Terning 1");
   }
   else {
     nyeTerninger[0] = 0;
-    console.log("Test: Ikke terning 1");
+    //console.log("Test: Ikke terning 1");
   }
   if (document.getElementById("check2").checked) {
     nyeTerninger[1] = 1;
-    console.log("Test: Terning 2");
+    //console.log("Test: Terning 2");
   }
   else {
     nyeTerninger[1] = 0;
-    console.log("Test: Ikke terning 2");
+    //console.log("Test: Ikke terning 2");
   }
   if (document.getElementById("check3").checked) {
     nyeTerninger[2] = 1;
-    console.log("Test: Terning 3");
+    //console.log("Test: Terning 3");
   }
   else {
     nyeTerninger[2] = 0;
-    console.log("Test: Ikke terning 3");
+    //console.log("Test: Ikke terning 3");
   }
   if (document.getElementById("check4").checked) {
     nyeTerninger[3] = 1;
-    console.log("Test: Terning 4");
+    //console.log("Test: Terning 4");
   }
   else {
     nyeTerninger[3] = 0;
-    console.log("Test: Ikke terning 4");
+    //console.log("Test: Ikke terning 4");
   }
   if (document.getElementById("check5").checked) {
     nyeTerninger[4] = 1;
-    console.log("Test: Terning 5");
+    //console.log("Test: Terning 5");
   }
   else {
     nyeTerninger[4] = 0;
-    console.log("Test: Ikke terning 5");
+    //console.log("Test: Ikke terning 5");
   }
-  console.log("Test: slutt nytt_kast_indexer " + nyeTerninger);
+  //console.log("Test: slutt nytt_kast_indexer " + nyeTerninger);
   nyttKast(nyeTerninger);
 }
