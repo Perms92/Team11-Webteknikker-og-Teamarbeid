e9fb702d2c11267301c915327175351cab78f198
Perms92 <permso@stud.ntnu.no>
2019-10-12 00:27:33 +0200

    Endret ID på checkboxer
    Forkortet nytt_kast_indexer med en for-løkke

--- Utkast 1 Yatzy/yatzy.js
+++ Utkast 1 Yatzy/yatzy.js
@@ -2,6 +2,7 @@ var mitt_kast = [];
 var antallKast = 0;
 var runde = 0;
 
+// En felles funksjon som endrer teksten på kasteknappen
 function kasteKnapp() {
   console.log("Test mitt_kast " + mitt_kast);
   var x = document.getElementById("kasteKnapp");
@@ -67,50 +68,18 @@ function nyttKast(indekser){
 // Definerer hvilke indekser som skal kastes på nytt
 function nytt_kast_indexer(){
   var nyeTerninger = [];
-  if (document.getElementById("check1").checked){
-    nyeTerninger[0] = 1;
-    //console.log("Test: Terning 1");
+  for (var i = 0; i < 4; i++) {
+    if (document.getElementById(i).checked) {
+        nyeTerninger[i] = 1;
+    }
+    else {
+      nyeTerninger[i] = 0;
+    }
   }
-  else {
-    nyeTerninger[0] = 0;
-    //console.log("Test: Ikke terning 1");
-  }
-  if (document.getElementById("check2").checked) {
-    nyeTerninger[1] = 1;
-    //console.log("Test: Terning 2");
-  }
-  else {
-    nyeTerninger[1] = 0;
-    //console.log("Test: Ikke terning 2");
-  }
-  if (document.getElementById("check3").checked) {
-    nyeTerninger[2] = 1;
-    //console.log("Test: Terning 3");
-  }
-  else {
-    nyeTerninger[2] = 0;
-    //console.log("Test: Ikke terning 3");
-  }
-  if (document.getElementById("check4").checked) {
-    nyeTerninger[3] = 1;
-    //console.log("Test: Terning 4");
-  }
-  else {
-    nyeTerninger[3] = 0;
-    //console.log("Test: Ikke terning 4");
-  }
-  if (document.getElementById("check5").checked) {
-    nyeTerninger[4] = 1;
-    //console.log("Test: Terning 5");
-  }
-  else {
-    nyeTerninger[4] = 0;
-    //console.log("Test: Ikke terning 5");
-  }
-  //console.log("Test: slutt nytt_kast_indexer " + nyeTerninger);
   nyttKast(nyeTerninger);
 }
 
+// Funksjonsnavnet er selvforklarende
 function rundeForteller(){
   console.log("Du er på runde " + (runde));
   if (runde < 7) {
