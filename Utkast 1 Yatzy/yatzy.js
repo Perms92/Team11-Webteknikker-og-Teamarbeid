46823a387902c8cc7fd07cada2f0683b4fbec52b
Perms92 <permso@stud.ntnu.no>
2019-10-11 23:24:45 +0200

    Bedre fungerende terninger
    Holder på de jeg vil, endrer etter 3 kast. Viser ikke siste kastet men heller nye kastet på neste runde

--- Utkast 1 Yatzy/yatzy.js
+++ Utkast 1 Yatzy/yatzy.js
@@ -1,54 +1,86 @@
 var mitt_kast = [];
 var antallKast = 0;
 var runde = 0;
-var score = 0;
-var midlertidigScore = 0;
 
-// Oppretter første kast hver runde
-function startKast(){
-  runde += 1;
-  rundeForteller();
-  // Skjer bare om det er første kast
-  if (antallKast == 0) {
-    // Lager 5 indekser/terninger
-    for (i = 0; i <= 4; i++){
-      // Setter random tall 1-6 på hver indeks/terning
-      mitt_kast[i] = Math.floor((Math.random() * 6) + 1);
-    }
-    // Teller startkastet som kast nr. 1
-    antallKast = 1;
-    console.log("Test: antallKast startkast " + antallKast);
-    // Returnerer arrayet med 5 terninger med random verdier
-    document.getElementById("kast").innerHTML = mitt_kast;
+function kasteKnapp() {
+  console.log("Du er på runde " + runde);
+  var x = document.getElementById("kasteKnapp");
+  if (x.innerHTML === "Neste kast" && antallKast != 2) {
+    console.log("Test 1 kasteKnapp()");
+    nytt_kast_indexer();
   }
-  else {
-    console.log("Bare første kast her");
+  else if (x.innerHTML === "Neste kast" && antallKast <= 2) {
+    antallKast = 0;
+    console.log("Test 2 kasteKnapp()");
+    x.innerHTML = "Start kast"
+    startKast();
+  }
+  else if (x.innerHTML === "Start kast") {
+    console.log("Test 4 kasteKnapp()");
+    x.innerHTML = "Neste kast";
+    startKast();
+  }
+  else if (x.innerHTML === "Start spillet") {
+    console.log("Test 5 kasteKnapp()");
+    x.innerHTML = "Neste kast";
+    startKast();
   }
 }
 
-// Bytter de terningene som ikke er huket i checkbox
-function nyttKast(indekser){
-  console.log(indekser + " Test indeks nyttKast");
-  if (antallKast > 0 && antallKast < 3) {
-    for (i = 0; i <= 4; i++){
-      if (indekser[i] == 0){
-        //console.log("Test: indekser [0] " + indekser[i]);
+// Oppretter første kast hver runde
+function startKast(){
+  if (antallKast == 0) {
+    runde += 1;
+    rundeForteller();
+    // Skjer bare om det er første kast
+    if (antallKast == 0) {
+      // Lager 5 indekser/terninger
+      for (i = 0; i <= 4; i++){
+        // Setter random tall 1-6 på hver indeks/terning
         mitt_kast[i] = Math.floor((Math.random() * 6) + 1);
       }
+      // Teller startkastet som kast nr. 1
+      antallKast = 1;
+      console.log("Test: antallKast startkast " + antallKast);
+      // Returnerer arrayet med 5 terninger med random verdier
+      document.getElementById("kast").innerHTML = mitt_kast;
+    }
+    else {
+      console.log("Bare første kast her");
+    }
+  }
+}
+
+
+// Bytter de terningene som ikke er huket i checkbox
+function nyttKast(indekser){
+  if (antallKast != 3) {
+    console.log("Test: antallKast != 3");
+    console.log(indekser + " Test indeks nyttKast");
+    if (antallKast > 0 && antallKast < 3) {
+      for (i = 0; i <= 4; i++){
+        if (indekser[i] == 0){
+          //console.log("Test: indekser [0] " + indekser[i]);
+          mitt_kast[i] = Math.floor((Math.random() * 6) + 1);
+        }
+      }
+      console.log("Test: antallKast " + antallKast);
+      if (antallKast == 3) {
+        //score(); Fungerer ikke som en funksjon, vet ikke hvorfor
+        console.log("Du har nå kastet 3 ganger, start på nytt");
+        antallKast = 0;
+      }
+    }
+    else {
+      console.log("Test: Du må starte spillet for å kaste på nytt");
+      console.log(antallKast);
     }
     antallKast += 1;
-    //console.log("Test: antallKast " + antallKast);
-    if (antallKast == 3) {
-      //score(); Fungerer ikke som en funksjon, vet ikke hvorfor
-      console.log("Du har nå kastet 3 ganger, start på nytt");
-      antallKast = 0;
-    }
-  }
-  else {
-    console.log("Test: Du må starte spillet for å kaste på nytt");
-    console.log(antallKast);
+    console.log("Test: antallKast " + antallKast);
   }
   document.getElementById("kast").innerHTML = mitt_kast;
+  //mitt_kast = [];
+  console.log("Test: reset mitt_kast " + mitt_kast);
 }
 
 
@@ -101,20 +133,8 @@ function nytt_kast_indexer(){
 
 function rundeForteller(){
   if (runde < 7) {
-    console.log("Du skal nå finne " + runde + "-ere");
-  }
-}
-
-function score(){
-  if (runde < 7) {
-    for (i = 0; i <= 4; i++) {
-      if (runde == mitt_kast[i]) {
-        midlertidigScore += mitt_kast[i];
-      }
+    for (i = 0; i <= 5; i++) {
+      console.log("Du vil nå ha " + runde + "-ere");
     }
-    console.log("Du scorte " + midlertidigScore + " for " + runde + "-ere");
-    score += midlertidigScore;
-    console.log("Din totale score er: " + score);
-    midlertidigScore = 0;
   }
 }
