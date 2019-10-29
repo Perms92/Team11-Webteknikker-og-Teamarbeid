dfa8c4dbd92ba8ec3b93db6ab57309dba3a0c4e8
minauthus <mina.uthus@gmail.com>
2019-10-29 20:43:04 +0100

    Endret regler

--- Utkast 1 Yatzy/menyer.js
+++ Utkast 1 Yatzy/menyer.js
@@ -1,20 +1,33 @@
-function regler(){
+let hidden = true;
+function regler() {
   var x = document.getElementById("regler");
-  if (x.style.display === "none") {
+  // STUDASS TANKER :)
+  // x.style,display = "none"; ved eventuelt onload, da vil den fungere igjen
+  if (!hidden) {
+    x.style.display = "none";
+    hidden = true;
+  } else {
+    x.style.display = "block";
+    hidden = false;
+  }
+  //STUDASS FERDIG
+  /*  if (x.style.display == "none") {
     x.style.display = "block";
   } else {
     x.style.display = "none";
   }
+  */
 }
 
-function playerName(){
+function playerName() {
   text = String(prompt("Skriv inn ditt spillernavn her!"));
   document.getElementById("spiller1").innerHTML += text + "";
   //text1 = String(prompt("Skriv inn ditt spillernavn her!"));
   //document.getElementById("spiller2").innerHTML += text1 + "";
-  document.getElementById('navneknapp').style.display="none";
-  document.getElementById("kasteKnapp").style.visibility="visible";
-  document.getElementById("brett").style.backgroundColor="#468f15";
-  document.getElementById("brett").style.backgroundImage="url('https://www.transparenttextures.com/patterns/60-lines.png')";
-  document.getElementById("brett").style.borderColor="saddlebrown #713F00";
+  document.getElementById("navneknapp").style.display = "none";
+  document.getElementById("kasteKnapp").style.visibility = "visible";
+  document.getElementById("brett").style.backgroundColor = "#468f15";
+  document.getElementById("brett").style.backgroundImage =
+    "url('https://www.transparenttextures.com/patterns/60-lines.png')";
+  document.getElementById("brett").style.borderColor = "saddlebrown #713F00";
 }
