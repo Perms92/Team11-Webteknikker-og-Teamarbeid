0533af135bd23b0bfd18a4d7c7dc9195365d21d0
minauthus <mina.uthus@gmail.com>
2019-10-30 11:39:14 +0100

    endret kommentar

--- Utkast 1 Yatzy/menyer.js
+++ Utkast 1 Yatzy/menyer.js
@@ -1,7 +1,6 @@
 let hidden = true;
 function regler() {
   var x = document.getElementById("regler");
-  // STUDASS TANKER :)
   // x.style,display = "none"; ved eventuelt onload, da vil den fungere igjen
   if (!hidden) {
     x.style.display = "none";
@@ -10,7 +9,6 @@ function regler() {
     x.style.display = "block";
     hidden = false;
   }
-  //STUDASS FERDIG
   /*  if (x.style.display == "none") {
     x.style.display = "block";
   } else {
@@ -19,7 +17,7 @@ function regler() {
   */
 }
 
-function playerName(){
+function playerName() {
   spiller1 = String(prompt("Skriv inn spiller 1 her"));
   let erTomt = "true";
   while (erTomt == "true") {
@@ -28,17 +26,17 @@ function playerName(){
       if (spiller1 != "") {
         erTomt = "false";
       }
-    }
-    catch(err) {
+    } catch (err) {
       spiller1 = String(prompt(err));
     }
   }
   document.getElementById("spiller1").innerHTML = spiller1 + "";
   spiller2 = String(prompt("Skriv inn spiller 2 eller la stå åpen"));
   document.getElementById("spiller2").innerHTML = spiller2 + "";
-  document.getElementById('navneknapp').style.display="none";
-  document.getElementById("kasteKnapp").style.visibility="visible";
-  document.getElementById("brett").style.backgroundColor="#468f15";
-  document.getElementById("brett").style.backgroundImage="url('https://www.transparenttextures.com/patterns/60-lines.png')";
-  document.getElementById("brett").style.borderColor="saddlebrown #713F00";
+  document.getElementById("navneknapp").style.display = "none";
+  document.getElementById("kasteKnapp").style.visibility = "visible";
+  document.getElementById("brett").style.backgroundColor = "#468f15";
+  document.getElementById("brett").style.backgroundImage =
+    "url('https://www.transparenttextures.com/patterns/60-lines.png')";
+  document.getElementById("brett").style.borderColor = "saddlebrown #713F00";
 }
