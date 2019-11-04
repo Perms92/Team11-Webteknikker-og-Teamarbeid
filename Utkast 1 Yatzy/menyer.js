8499e0ec3a0e5ebdf869856df7e22956fa282c92
minauthus <mina.uthus@gmail.com>
2019-11-04 14:13:22 +0100

    firebase, funker ikke

--- Utkast 1 Yatzy/menyer.js
+++ Utkast 1 Yatzy/menyer.js
@@ -55,7 +55,7 @@ function playerName() {
   document.getElementById("brett").style.borderColor = "saddlebrown #713F00";
 }
 
-let testpersoner = [
+/*let testpersoner = [
   { navn: "Peder", score: 123 },
   { navn: "Andy", score: 156 }
 ];
@@ -109,4 +109,35 @@ function renderHighscores() {
   }
 }
 
-renderHighscores();
+renderHighscores();*/
+
+/*var database = firebase.database();
+var yatzyDB = database.ref("yatzy/");
+var liste = document.getElementById("liste");
+
+var hsPopUp = document.getElementById("hsPopUp");
+var hs = document.getElementById("hs");
+
+function hentHS(snapshot) {
+  var yatzy = snapshot.key;
+}
+yatzyDB.orderByChild("score").on("child_added", hentHS);
+
+function visHighscore() {
+  var navn = snapshot.key;
+  var score = snapshot.val().alder;
+
+  liste.innerHTML += `<ol>
+<li>${navn} fikk ${score} poeng </li>
+  </ol>`;
+}
+
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="/__/firebase/7.2.3/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="/__/firebase/7.2.3/firebase-analytics.js"></script>
+
+<!-- Initialize Firebase -->
+<script src="/__/firebase/init.js"></script>*/
