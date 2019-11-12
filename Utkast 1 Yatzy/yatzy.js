89d3234fa66791e699d5c447d3a65e63131eafda
Perms92 <permso@stud.ntnu.no>
2019-11-12 15:26:11 +0100

    Navneendring

--- Utkast 1 Yatzy/yatzy.js
+++ Utkast 1 Yatzy/yatzy.js
@@ -14,7 +14,6 @@ var spiller4 = "";
 //var spillere = [];
 var hvemSinTur = 1;
 var drop = document.getElementById("diceSound");
-var diceroll = document.getElementById("rollsound");
 let hiddenPopFinalScore = true;
 
 function antallAvatarer() {
@@ -36,23 +35,23 @@ function antallAvatarer() {
 
 function kasteKnapp() {
   //console.log("Test mitt_kast " + mitt_kast);
+  var diceroll = document.getElementById("rollsound");
+  diceroll.play();
 
   var x = document.getElementById("kasteKnapp");
   if (x.innerHTML === "Neste kast" && antallKast != 2) {
     antallKast += 1;
+<<<<<<< HEAD
     ////console.log("Test 1 kasteKnapp()");
+=======
+    //console.log("Test 1 kasteKnapp()");
+>>>>>>> parent of 4cb49b6... fikset higlight av avatarer
     nytt_kast_indexer();
     showDice();
   } else if (x.innerHTML === "Neste kast" && antallKast == 2) {
     antallKast = 0;
     nytt_kast_indexer();
-    highlightAvatar();
-    drop.play();
-    // resetter terninger etter siste kast
-    for (i=0; i <= 4; i++){
-    document.getElementById("dicePos" + i).style.marginBottom = "0%";
-  }
-
+    //console.log("Test 2 kasteKnapp()");
     x.innerHTML = "Start kast";
     nytt_kast_indexer();
     showDice();
@@ -83,26 +82,26 @@ function kasteKnapp() {
     }
     //viseHvemSinTur();
   } else if (x.innerHTML === "Start kast") {
+<<<<<<< HEAD
     if (runde == 0) {
       runde = 1;
       hvemSinTur = 1;
     }
+=======
+>>>>>>> parent of 4cb49b6... fikset higlight av avatarer
     antallKast += 1;
     ////console.log("Test 4 kasteKnapp()");
     x.innerHTML = "Neste kast";
     startKast();
     showDice();
-    diceroll.play();
-  } else if (x.innerHTML === "Start" || x.innerHTML === "Nytt spill") {
+  } else if (x.innerHTML === "Start runde 1" || x.innerHTML === "Nytt spill") {
     if (x.innerHTML === "Nytt spill") {
       finalScore();
+      resetSpill();
     }
     antallKast += 1;
     ////console.log("Test 5 kasteKnapp()");
     x.innerHTML = "Neste kast";
-
-    diceroll.play();
-    highlightAvatar();
     startKast();
     showDice();
   }
@@ -124,18 +123,28 @@ function startKast() {
   ////console.log("Test: antallKast startkast " + antallKast);
   // Returnerer arrayet med 5 terninger med random verdier
   document.getElementById("kast").innerHTML = mitt_kast;
-  for (i=0; i <= 4; i++){
-    document.getElementById("dicePos" + i).style.transition = "all 0.3s";
-    document.getElementById("dicePos" + i).style.marginBottom = "0%";
-    document.getElementById(i).checked = false;
-  }
-
+  document.getElementById("dicePos0").style.transition = "all 0.3s";
+  document.getElementById("dicePos0").style.marginBottom = "0%";
+  document.getElementById("dicePos1").style.transition = "all 0.3s";
+  document.getElementById("dicePos1").style.marginBottom = "0%";
+  document.getElementById("dicePos2").style.transition = "all 0.3s";
+  document.getElementById("dicePos2").style.marginBottom = "0%";
+  document.getElementById("dicePos3").style.transition = "all 0.3s";
+  document.getElementById("dicePos3").style.marginBottom = "0%";
+  document.getElementById("dicePos4").style.transition = "all 0.3s";
+  document.getElementById("dicePos4").style.marginBottom = "0%";
   //putter styling av rundeforteller i js slik at man ikke ser deler av rundefortelleren før start spill er trykket
   document.getElementById("rundeForteller").style.border = "solid";
   document.getElementById("rundeForteller").style.borderColor = "#DBAD6A";
   document.getElementById("rundeForteller").style.borderWidth =
     "0px 0px 2px 0px";
   document.getElementById("rundeForteller").style.padding = "1%";
+
+  document.getElementById(0).checked = false;
+  document.getElementById(1).checked = false;
+  document.getElementById(2).checked = false;
+  document.getElementById(3).checked = false;
+  document.getElementById(4).checked = false;
 }
 
 // Bytter de terningene som ikke er huket i checkbox
@@ -780,30 +789,4 @@ function finalScore(nyttEllerGjenta) {
     hvemSinTur = 0;
     //console.log("Test nyttEllerGjenta: " + nyttEllerGjenta);
   }
-}
-function highlightAvatar() {
-  let x = "";
-  let y = "";
-  //kjører en løkke som setter alle style verdiene til orginalt. visibility er nødvendig slik at de ikke går tilbake til originalverdi (hidden)
-  for (var i = 1; i <= antallSpillere; i++) {
-    let y = "imgAvatar" + i;
-    document.getElementById(y).style = "width: 8rem; height: 8rem;";
-    document.getElementById(y).style.visibility = "visible";
-    document.getElementById(y).style.opacity = "0.5";
-    console.log("Test higlightavatar: " + y);
-  }
-
-  if (hvemSinTur == antallSpillere || (hvemSinTur == 1 && runde == 0)) {
-    let knapp = document.getElementById("kasteKnapp");
-    x = "imgAvatar" + 1;
-    z = "avatar" + 1;
-  } else {
-    let knapp = document.getElementById("kasteKnapp");
-    x = "imgAvatar" + (hvemSinTur+1);
-    z = "avatar" + (hvemSinTur+1);
-  }
-  //highlighter avatar
-  document.getElementById(x).style = "width: 9.5rem; height: 9.5rem;";
-  document.getElementById(x).style.visibility = "visible";
-  console.log("Test highlight avatar: " + x);
 }
